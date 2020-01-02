b9bf395c7abced5f483202f4d6908078274f0104
Nico Vermaas <vermaas@astron.nl>
2020-01-02 10:55:12 +0100

    adding to readme

--- adex/api/models.py
+++ adex/api/models.py
@@ -10,6 +10,8 @@ class DataSource(models.Model):
     instrument = models.CharField(max_length=30, default="unknown") # WSRT, Apertif, LOFAR
     description = models.CharField(max_length=1000, default="unknown")
     thumbnail = models.CharField(max_length=200, default="https://alta.astron.nl/alta-static/unknown.jpg")
+    url_to_archive = models.CharField(max_length=200, default="")
+
 
     # the representation of the value in the REST API
     def __str__(self):
