7f671b5b1dd4b493f6c0529a02dc9abc3da84e29
Nico Vermaas <vermaas@astron.nl>
2020-03-20 06:50:23 +0100

    adding configurability

--- esap/api/business/configuration.py
+++ esap/api/business/configuration.py
@@ -1,13 +1,16 @@
-import sys
+import os, sys
 from django.conf import settings
 
 import importlib
-sys.path.append(settings.CONFIGURATION_DIR)
 
-my_config = importlib.import_module(settings.CONFIGURATION_FILE)
+try:
+    sys.path.append(settings.CONFIGURATION_DIR)
+    my_config = importlib.import_module(settings.CONFIGURATION_FILE)
+except:
+    # no configuration definition found in settings.py, use the default configuration
+    esap_default_config = os.path.join((os.path.join(settings.BASE_DIR, 'configuration'),'esap_default'))
+    my_config = importlib.import_module(esap_default_config)
 
-#my_config = __import__('dev')
-#from esap.esap.configuration import esap_dev as my_config
 
 # return expanded configuration
 def get_configuration():
--- esap/esap/configuration/esap_default.py
+++ esap/esap/configuration/esap_default.py
@@ -1,4 +1,18 @@
-{
+
+#title = "ESAP (dev)"
+#logo = "http://uilennest.net/static/media/tree9.da598501.png"
+logo = "https://alta.astron.nl/alta-static/images/esap/esap_logo.png"
+
+# definition of the navigation bar
+nav1 = {'title': 'Archives', 'route': '/archives'}
+nav2 = {'title': 'Datasets', 'route': '/datasets'}
+nav3 = {'title': 'Telescopes', 'route': '/telescopes'}
+nav4 = {'title': 'Vraagjes', 'route': '/query'}
+nav5 = {'title': 'About', 'route': '/about'}
+navbar = [nav1,nav2,nav3,nav4]
+
+# definition of the query
+query_schema = {
   "title": "ESAP Query",
   "type": "object",
   "properties": {
@@ -61,7 +75,8 @@
     },
     "instrument": {
       "type": "string",
-      "title": "Instrument"
+      "title": "Instrumentje"
     }
   }
 }
+
