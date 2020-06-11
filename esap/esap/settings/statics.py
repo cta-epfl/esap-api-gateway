896fb90e4b9d9fb423b31aa78871a2b1547e1f31
Nico Vermaas <vermaas@astron.nl>
2020-06-11 08:07:32 +0200

    switch Docker base image from python:3.6.7-alpine to python:3.6.7-slim

---
+++ esap/esap/settings/statics.py
@@ -0,0 +1,26 @@
+"""
+Django settings for esap project.
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'cie-((m#n$br$6l53yash45*2^mwuux*2u)bad5(0flx@krnj9'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+INSTALLED_APPS = [
+    'api',
+    'django.contrib.staticfiles',
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
