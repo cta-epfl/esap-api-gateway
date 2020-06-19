14a207b9ec684f6c0b7ee2f85cf159e7deb2e408
Nico Vermaas <vermaas@astron.nl>
2020-06-19 16:09:01 +0200

    - switch production version to adex.py

--- esap/esap/settings/prod.py
+++ esap/esap/settings/prod.py
@@ -23,4 +23,4 @@ SECRET_KEY = 'cie-((m#n$br$6l53yash45*2^mwuux*2u)bad5(0flx@krnj9'
 
 # configuration settings that can be requested through the REST API
 CONFIGURATION_DIR = os.path.join(BASE_DIR, 'configuration')
-CONFIGURATION_FILE = 'esap_default'
+CONFIGURATION_FILE = 'adex'
