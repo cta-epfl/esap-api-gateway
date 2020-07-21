5687d7fb1a5cd69fee7fc414853dd1b880c44523
Nico Vermaas <vermaas@astron.nl>
2020-07-21 09:12:30 +0200

    alta.py service connector, switch to dataproducts query instead of observations query

--- esap/query/api/services/alta.py
+++ esap/query/api/services/alta.py
@@ -29,7 +29,7 @@ class alta_connector(query_base):
 
     # Initializer
     def __init__(self, url):
-        self.url = url
+        self.url = url + '/dataproducts'
 
 
     # construct a query for this type of service
@@ -65,21 +65,15 @@ class alta_connector(query_base):
             where = where + AMP_REPLACEMENT
 
         if 'IMAGING' in dataset.collection.upper():
-            # instrument__icontains=APERTIF
+
             if 'RAW' in dataset.level.upper():
                 where = where + "dataProductSubType__in=uncalibratedVisibility"
 
             if 'PROCESSED' in dataset.level.upper():
-
-                # these are not observations, but pipelines. Access activities
-                self.url = self.url.replace('observations','activities')
                 where = where + "dataProductSubType__in=calibratedVisibility,continuumMF,continuumChunk,imageCube,beamCube,polarisationImage,polarisationCube,continuumCube"
 
         if 'TIMEDOMAIN' in dataset.collection.upper():
-            where = where + "project__externalRef=ARTSSC"
-            where = where + "dataProductSubType=pulsarTimingTimeSeries"
-            # instrument__icontains=ARTS
-            # http://localhost/altapi/activities?project__externalRef=ARTSSC
+              where = where + "dataProductSubType=pulsarTimingTimeSeries"
 
         # if query ends with a separation character then cut it off
         if where.endswith(AMP_REPLACEMENT):
@@ -98,8 +92,8 @@ class alta_connector(query_base):
         :return: results: an array of dicts with the following structure;
 
          example:
-        /esap-api/run-query/?dataset_uri=apertif-imaging-rawdata&query=https://alta.astron.nl/altapi/observations-flat?view_ra=342.16_and_view_dec=33.94_and_view_fov=10_and_dataProductType=image_and_dataProductSubType=continuumMF
-
+        /esap-api/run-query/?dataset_uri=apertif-imaging-processeddata
+        &query='https://alta.astron.nl/altapi/dataproducts?view_ra=342.16_and_view_dec=33.94_and_view_fov=10_and_dataProductSubType__in=calibratedVisibility,continuumMF,continuumChunk,imageCube,beamCube,polarisationImage,polarisationCube,continuumCube
         """
 
         results = []
@@ -110,46 +104,13 @@ class alta_connector(query_base):
 
         try:
 
-            # execute the first http request to ALTA to do the cone search on observation level.
+            # execute the http request to ALTA and retrieve the dataproducts
             response = requests.request("GET", query, headers=ALTA_HEADER)
-
-            json_response = json.loads(response.text)
-            observations = json_response["results"]
-
-            logger.info('observations in response: '+str(len(observations)))
-
-            # iterate over the list of results.. and gather the runid's in a comma separated list for the next query on datasetid
-            list = []
-            for observation in observations:
-                list.append(observation['runId'])
-
-            runids = ','.join(list)
-
-            # construct a second query on dataproducts for the gathered runId's
-            host = ALTA_HOST
-            if "/observations" in query:
-                query_list = query.split('/observations')
-                host = query_list[0]
-
-            elif "/activities" in query:
-                query_list = query.split('/activities')
-                host = query_list[0]
-
-
-            # there may be additional query parameters in the original query, like dataProductSubType=continuumMF
-            # copy them over to the secondary query as well.
-            filter = '&' + query_list[1].split('?')[1]
-            dataproduct_query = host + '/dataproducts-flat?datasetID__in=' + str(runids) + filter
-
-            # shortcut to add an extra selection criterium... handle better later
-            # dataproduct_query = dataproduct_query + "&dataProductSubType=continuumMF"
-
-            # execute the secondary query to dataproducts
-            response = requests.request("GET", dataproduct_query, headers=ALTA_HEADER)
 
             json_response = json.loads(response.text)
             dataproducts = json_response["results"]
 
+            logger.info('nr of dataproducts in response: '+str(len(dataproducts)))
 
             for dataproduct in dataproducts:
 
@@ -161,21 +122,21 @@ class alta_connector(query_base):
                 record['dataProductType'] = dataproduct['dataProductType']
                 record['dataProductSubType'] = dataproduct['dataProductSubType']
 
-                # result = "https://alta.astron.nl/science/details/"+observation["runId"]
                 record['generatedByActivity'] = dataproduct['generatedByActivity'][0]
                 record['datasetID'] = dataproduct['datasetID']
                 # record['target'] = "???"
                 record['RA'] = dataproduct['RA']
                 record['dec'] = dataproduct['dec']
                 record['fov'] = dataproduct['fov']
+                record['release'] = dataproduct['derived_release_id']
 
-
-                # only send back thumbnails that are not placeholders.
+                # only send back thumbnails that are not static placeholders.
                 if record['dataProductSubType']=='continuumMF':
                     record['thumbnail'] = dataproduct['thumbnail']
 
                 record['storageRef'] = dataproduct['storageRef']
-                # construct the url based on the storageRef
+
+                # construct the url to the data based on the storageRef
                 record['url'] = ALTA_WEBDAV_HOST + dataproduct['derived_release_id'] + '/' + dataproduct['storageRef']
 
                 results.append(record)
