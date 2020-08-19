57361593d79c8188ac92c07dbe83112cd2293b76
meyer <meyer@astron.nl>
2020-08-19 21:47:52 +0200

    updated ivoa query schema

--- esap/esap/configuration/esap_ivoa.py
+++ esap/esap/configuration/esap_ivoa.py
@@ -1,6 +1,6 @@
 
 # title = "ESFRI Science Analysis Platform"
-#logo = "http://uilennest.net/static/media/tree9.da598501.png"
+# logo = "http://uilennest.net/static/media/tree9.da598501.png"
 
 # the url location of the frontend application,
 # this makes it possible to install multiple instances in different directories on the webserver
@@ -17,7 +17,7 @@ nav4 = {'title': 'Interactive Analysis', 'route': '/interactive'}
 navbar = [nav1, nav2, nav3, nav4]
 
 # if datasets_enabled is set, then only these datasets are visible to the GUI
-#datasets_enabled = ['apertif-observations','astron.ivoa.obscore']
+# datasets_enabled = ['apertif-observations','astron.ivoa.obscore']
 
 # if datasets_disabled is set, then all datasets except these are returned to the GUI
 datasets_disabled = ['nancay.ivoa.obscore']
@@ -31,7 +31,7 @@ query_schema = {
     "properties": {
         "service_type": {
             "type": "string",
-            "title": "Service",
+            "title": "Service Protocol",
             "default": "tap",
             "enum": ["tap", "scs", "ssa", "sia"],
             "enumNames": ["TAP: Tables", "SCS: Cone Search", "SSA: Spectra", "SIA: Images"]
@@ -41,16 +41,58 @@ query_schema = {
             "title": "Keyword",
             "default": ""
         },
+        "service_list": {
+            "type": "array",
+            "title": "List of services",
+            "items": {
+                "type": "string",
+            }
+        }
+    },
 
-        # hidden property, keep it at the end of the properties list
-        "catalog": {
-            "type": "string",
-            "title": "Catalog",
-            "default": "vo_reg",
-            "enum": ["vo_reg"],
-            "enumNames": ["IVOA"],
+    "required": ["service_type"],
+
+    "dependencies": {
+        "service_type": {
+            "oneOf": [
+                {
+                    "properties": {
+                        "service_type": {"enum": ["tap"]},
+                        "adql_query": {
+                            "type": "string",
+                            "title": "ADQL Query",
+                        },
+                        "tap_schema": {
+                            "type": "string",
+                            "title": "TAP_SCHEMA",
+                            "enum": ["TAP_SCHEMA.schemas", "TAP_SCHEMA.tables", "TAP_SCHEMA.columns"],
+                        }
+                    },
+                },
+                {
+                    "properties": {
+                        "service_type": {"enum": ["scs"]},
+                        "ra": {
+                            "type": "number",
+                            "title": "RA (degrees)",
+                        },
+                        "dec": {
+                            "type": "number",
+                            "title": "dec (degrees)",
+                        },
+                        "fov": {
+                            "type": "number",
+                            "title": "search radius (degrees)",
+                        },
+                        "url": {
+                            "type": "string",
+                            "title": "Service URL",
+                        }
+                    }
+                }
+            ]
         },
-    }
+    },
 }
 
-ui_schema = {"catalog": {"ui:widget": "hidden"}}
+ui_schema = {"adql_query": {"ui:widget": "textarea"}}
