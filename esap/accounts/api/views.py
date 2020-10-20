c7894b7e2f41e99c87cfaea76931604b08f4b89a
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2020-10-20 09:01:13 +0100

    Added overly lax permissions and changed queryset logic for user profile.

--- esap/accounts/api/views.py
+++ esap/accounts/api/views.py
@@ -10,6 +10,7 @@ class EsapQuerySchemaViewSet(viewsets.ModelViewSet):
 
     queryset = EsapQuerySchema.objects.all().order_by("schema_name")
     serializer_class = EsapQuerySchemaSerializer
+    permission_classes = []
 
 
 class EsapComputeResourceViewSet(viewsets.ModelViewSet):
@@ -19,6 +20,7 @@ class EsapComputeResourceViewSet(viewsets.ModelViewSet):
 
     queryset = EsapComputeResource.objects.all().order_by("resource_name")
     serializer_class = EsapComputeResourceSerializer
+    permission_classes = []
 
 
 class EsapSoftwareRepositoryViewSet(viewsets.ModelViewSet):
@@ -28,6 +30,7 @@ class EsapSoftwareRepositoryViewSet(viewsets.ModelViewSet):
 
     queryset = EsapSoftwareRepository.objects.all().order_by("repository_name")
     serializer_class = EsapSoftwareRepositorySerializer
+    permission_classes = []
 
 
 class EsapShoppingItemViewSet(viewsets.ModelViewSet):
@@ -37,6 +40,7 @@ class EsapShoppingItemViewSet(viewsets.ModelViewSet):
 
     queryset = EsapShoppingItem.objects.all()
     serializer_class = EsapShoppingItemSerializer
+    permission_classes = []
 
 
 class EsapUserProfileViewSet(viewsets.ModelViewSet):
@@ -46,7 +50,9 @@ class EsapUserProfileViewSet(viewsets.ModelViewSet):
 
     queryset = EsapUserProfile.objects.all().order_by("user_name")
     serializer_class = EsapUserProfileSerializer
+    permission_classes = []
 
     def get_queryset(self):
+        # Returns nothing if no user_name supplied instead of all
         user_name = self.request.query_params.get("user_name", None)
         return EsapUserProfile.objects.filter(user_name=user_name)
