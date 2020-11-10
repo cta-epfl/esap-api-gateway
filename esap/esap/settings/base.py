f3e27f2f6c2a92211395704aebbe7af489150d00
Nico Vermaas <vermaas@astron.nl>
2020-11-10 16:38:28 +0100

    changing IAM redirect url

--- esap/esap/settings/base.py
+++ esap/esap/settings/base.py
@@ -230,6 +230,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # configuration settings that can be requested through the REST API
-VERSION = "ASTRON - version 6 nov 2020"
+VERSION = "ASTRON - version 10 nov 2020"
 CONFIGURATION_DIR = os.path.join(BASE_DIR, 'configuration')
 CONFIGURATION_FILE = 'esap_default'
