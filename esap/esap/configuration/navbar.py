49ba28740e48c59a9cf2c988413902ad1db34a74
Nico Vermaas <vermaas@astron.nl>
2020-12-16 07:18:27 +0100

    deploy new version with navbar changes

--- esap/esap/configuration/navbar.py
+++ esap/esap/configuration/navbar.py
@@ -7,4 +7,5 @@ nav1 = {'title': 'Archives', 'route': '/archives'}
 nav2 = {'title': 'Query', 'route': '/vo-query'}
 nav3 = {'title': 'Rucio', 'route': '/rucio'}
 nav4 = {'title': 'Interactive Analysis', 'route': '/interactive'}
-navbar = [nav1, nav2, nav3, nav4]
+nav5 = {'title': 'SAMP', 'route': '/samp'}
+navbar = [nav1, nav2, nav3, nav4, nav5]
