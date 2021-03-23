d91c19104e4b9e26e5274fd70ece628952219e9d
Nico Vermaas <vermaas@astron.nl>
2021-03-23 09:54:29 +0100

    modifying CI/CD pipelines

--- esap/accounts/api/views.py
+++ esap/accounts/api/views.py
@@ -1,10 +1,11 @@
+import logging
+from django.contrib import auth
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import *
 from ..models import *
 
-from django.contrib import auth
-
+logger = logging.getLogger(__name__)
 
 class EsapQuerySchemaViewSet(viewsets.ModelViewSet):
     """
@@ -57,16 +58,20 @@ class EsapUserProfileViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # Returns nothing if no user_name supplied instead of all
+        print("EsapUserProfileViewSet.get_queryset()")
+
         try:
-            # id_token = self.request.session["oidc_id_token"]
-            # uid = id_token["iss"]+id_token["sub"]
-            # preferred_username = id_token["preferred_username"]
-            # name = id_token["name"]
-            # access_token = self.request.session["oidc_access_token"]
+            id_token = self.request.session["oidc_id_token"]
+
+            #uid = id_token["iss"]+id_token["sub"]
+            #preferred_username = id_token["preferred_username"]
+            #name = id_token["name"]
+            #access_token = self.request.session["oidc_access_token"]
             user = auth.get_user(self.request)
             user_email = user.email
             return EsapUserProfile.objects.filter(user_email=user_email)
 
         except AttributeError as e:
+            print('ERROR: '+str(e))
             user_name = self.request.query_params.get("user_name", None)
             return EsapUserProfile.objects.filter(user_name=user_name)
