b8fca10d98f237b879d807959d7e013683893dce
Fanna Lautenbach <lautenbach@astron.nl>
2022-06-13 15:54:51 +0200

    change adex config file, remove redundant adex elements

--- esap/esap/configuration/adex.py
+++ esap/esap/configuration/adex.py
@@ -1,37 +1,135 @@
-logo = "https://alta.astron.nl/alta-static/images/esap/adex_logo_clear.png"
-
-
-# definition of the query
-query_schema = {
-    "name": "adex",
-    "title": "ASTRON Data Collection Query",
-    "type": "object",
-    "properties": {
-        "catalog": {
-            "type": "string",
-            "title": "Catalog",
-            "default": "adex",
-            "enum": ["adex", "apertif", "astron_vo", "lofar"],
-            "enumNames": ["All", "Apertif", "ASTRON_VO", "LOFAR"],
-        },
-        "target": {
-            "type": "string",
-            "title": "Target"
-        },
-        "ra": {
-            "type": "number",
-            "title": "RA (degrees)",
-            "default": 342.16,
-        },
-        "dec": {
-            "type": "number",
-            "title": "dec (degrees)",
-            "default": 33.94,
-        },
-        "fov": {
-            "type": "number",
-            "title": "search radius (degrees)",
-            "default": 2.0,
-        },
+settings = {
+    "defaults": {
+        "survey": "apertif_dr1",
+        "archive": "astron_vo",
+        "collection": "apertif_dr1",
+        "calibration_level" : "processed",
+        "dataproduct_type": "image",
+        "skyCoords" : [340.0,34.0],
+        "fov": 15,
+        "pageSize" : 1000,
+        "refreshFactor" : 0.5
     },
+    "calibration_levels" : [
+        {
+            "value": "raw",
+            "label": "Raw",
+        },
+        {
+            "value": "processed",
+            "label": "Processed",
+        },
+    ],
+    "dataproduct_types": [
+        {
+            "value": "visibility",
+            "label": "Visibility",
+        },
+        {
+            "value": "image",
+            "label": "Image",
+        },
+        {
+            "value": "cube",
+            "label": "Cube",
+        },
+    ],
+    "surveys" : [
+        {
+            "hips_id" : "apertif_dr1",
+            "hips_name" : "apertif_dr1",
+            "hips_url" : "https://hips.astron.nl/ASTRON/P/apertif_dr1",
+            "hips_frame" : "equatorial",
+            "max_order" : 13,
+            "format" : "png"
+        },
+        {
+            "hips_id": "lotss_dr1_low",
+            "hips_name": "lotss_dr1_low",
+            "hips_url": "https://hips.astron.nl/ASTRON/P/lotss_dr1_low",
+            "hips_frame": "equatorial",
+            "max_order": 13,
+            "format": "png"
+        },
+        {
+            "hips_id": "lotss_dr1_high",
+            "hips_name": "lotss_dr1_high",
+            "hips_url": "https://hips.astron.nl/ASTRON/P/lotss_dr1_high",
+            "hips_frame": "equatorial",
+            "max_order": 13,
+            "format": "png"
+        },
+        {
+            "hips_id": "tgssadr",
+            "hips_name": "tgssadr",
+            "hips_url": "https://hips.astron.nl/ASTRON/P/tgssadr",
+            "hips_frame": "equatorial",
+            "max_order": 13,
+            "format": "png"
+        },
+        {
+            "hips_id": "lotss_dr2_low",
+            "hips_name": "lotss_dr2_low",
+            "hips_url": "https://hips.astron.nl/ASTRON/P/lotss_dr2_low",
+            "hips_frame": "equatorial",
+            "max_order": 13,
+            "format": "png"
+        },
+        {
+            "hips_id": "lotss_dr2_high",
+            "hips_name": "lotss_dr2_high",
+            "hips_url": "https://hips.astron.nl/ASTRON/P/lotss_dr2_high",
+            "hips_frame": "equatorial",
+            "max_order": 13,
+            "format": "png"
+        },
+    ],
+    "collections": [
+        {
+            "archive": "apertif",
+            "collection": "imaging",
+            "label" : "Imaging (Apertif)"
+        },
+        {
+            "archive": "apertif",
+            "collection": "timedomain",
+            "label": "Time Domain (Apertif)"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "apertif-dr1",
+            "label": "apertif-dr1"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "lotss-dr1",
+            "label": "lotss-dr1"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "lotss-pdr",
+            "label": "lotss-pdr"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "sauron",
+            "label": "sauron"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "tgssadr",
+            "label": "tgssadr"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "MSSSVerification",
+            "label": "MSSSVerification"
+        },
+        {
+            "archive": "astron_vo",
+            "collection": "hetdex",
+            "label": "hetdex"
+        },
+    ],
 }
+
