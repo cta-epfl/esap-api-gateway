02db7655c1f7cfaa35ccfb872126759e94d19c19
Fanna Lautenbach <lautenbach@astron.nl>
2022-06-30 15:02:18 +0200

    split datasets for dataproduct types

--- esap/esap/configuration/adex.py
+++ esap/esap/configuration/adex.py
@@ -43,82 +43,119 @@ settings = {
     ],
     "datasets": [
         {"lofar-lta": {
+            "uri": "lofar-lta",
             "archive": "lofar",
             "collection": "lofar",
             "level": "raw",
-            "dataproduct_type": ["visibility"],
+            "dataproduct_type": "visibility",
             "freq_min": 10,
             "freq_max": 240
         }},
-        {"astron_vo-apertif-dr1": {
+        {"astron_vo-apertif-dr1-cube": {
+            "uri": "astron_vo-apertif-dr1",
             "archive": "astron_vo",
             "collection": "apertif-dr1",
             "level": "processed",  # = 3
-            "dataproduct_type": ["cube", "image"],
+            "dataproduct_type": "cube",
+            "freq_min": 1338,
+            "freq_max": 1415
+        }},
+        {"astron_vo-apertif-dr1-image": {
+            "uri": "astron_vo-apertif-dr1",
+            "archive": "astron_vo",
+            "collection": "apertif-dr1",
+            "level": "processed",  # = 3
+            "dataproduct_type": "image",
             "freq_min": 1338,
             "freq_max": 1415
         }},
         {"sauron-hi-survey": {
+            "uri": "sauron-hi-survey",
             "archive": "astron_vo",
             "collection": "sauron",
             "level": "processed",  # = 0
-            "dataproduct_type": ["image"],
+            "dataproduct_type": "image",
             "freq_min": 1410,
             "freq_max": 1430
         }},
         {"msss-verification": {
+            "uri": "msss-verification",
             "archive": "astron_vo",
             "collection": "MSSSVerification",
             "level": "processed",  # = 0
-            "dataproduct_type": ["image"],
+            "dataproduct_type": "image",
             "freq_min": 30,
             "freq_max": 190
         }},
         {"tgssadr-image": {
+            "uri": "tgssadr-image",
             "archive": "astron_vo",
             "collection": "tgssadr",
             "level": "processed",  # = 0
-            "dataproduct_type": ["image"],
+            "dataproduct_type": "image",
             "freq_min": 147,
             "freq_max": 147
         }},
         {"lotss-dr1": {
+            "uri": "lotss-dr1",
             "archive": "astron_vo",
             "collection": "lotss-dr1",
             "level": "processed",  # = 3
-            "dataproduct_type": ["image"],
+            "dataproduct_type": "image",
             "freq_min": 120,
             "freq_max": 168
         }},
         {"lotss-pdr": {
+            "uri": "lotss-pdr",
             "archive": "astron_vo",
             "collection": "lotss-pdr",
             "level": "processed",  # = 0
-            "dataproduct_type": ["image"],
+            "dataproduct_type": "image",
             "freq_min": 120,
             "freq_max": 168
         }},
         {"apertif-imaging-rawdata": {
+            "uri": "apertif-imaging-rawdata",
             "archive": "apertif",
             "collection": "imaging",
             "level": "raw",  # = 0
-            "dataproduct_type": ["visibility"],
+            "dataproduct_type": "visibility",
             "freq_min": 1338,
             "freq_max": 1415
         }},
-        {"apertif-imaging-processeddata": {
+        {"apertif-imaging-processeddata-cube": {
+            "uri": "apertif-imaging-processeddata",
             "archive": "apertif",
             "collection": "imaging",
             "level": "processed",  # = 1, 2,
-            "dataproduct_type": ["cube", "image", "visibility"],
+            "dataproduct_type": "cube",
+            "freq_min": 1338,
+            "freq_max": 1415
+        }},
+        {"apertif-imaging-processeddata-image": {
+            "uri": "apertif-imaging-processeddata",
+            "archive": "apertif",
+            "collection": "imaging",
+            "level": "processed",  # = 1, 2,
+            "dataproduct_type": "image",
+            "freq_min": 1338,
+            "freq_max": 1415
+        }},
+        {"apertif-imaging-processeddata-visibility": {
+            "uri": "apertif-imaging-processeddata",
+            "archive": "apertif",
+            "collection": "imaging",
+            "level": "processed",  # = 1, 2,
+            "dataproduct_type": "visibility",
             "freq_min": 1338,
             "freq_max": 1415
         }},
         {"apertif-timedomain-raw": {
+            "uri": "apertif-timedomain-raw",
             "archive": "apertif",
             "collection": "timedomain",
             "level": "raw",  # = 0
-            "dataproduct_type": ["timeSeries"],
+            "dataproduct_type": "timeSeries",
             "freq_min": 1338,
             "freq_max": 1415
         }},
